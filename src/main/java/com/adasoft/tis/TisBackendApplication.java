9139b9b67b12139e9c1a7e5435508b7b09db77a9
jesusjimeneztapia <jesusjimeneztapia456@gmail.com>
2021-09-28 14:32:47 -0400

    Creación del proyecto.

---
+++ src/main/java/com/adasoft/tis/TisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adasoft.tis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TisBackendApplication.class, args);
+    }
+
+}
