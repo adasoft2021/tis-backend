44f67f8c05ebe11bf576b27c180c6675f9bc8a2e
jesusjimeneztapia <jesusjimeneztapia456@gmail.com>
2021-10-05 14:42:39 -0400

    Creación del repositorio de Qualification.

---
+++ src/main/java/com/adasoft/tis/repository/impl/QualificationRepositoryImpl.java
@@ -0,0 +1,18 @@
+package com.adasoft.tis.repository.impl;
+
+import com.adasoft.tis.core.repository.AbstractTisRepository;
+import com.adasoft.tis.domain.Qualification;
+import com.adasoft.tis.repository.QualificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+public class QualificationRepositoryImpl extends AbstractTisRepository<Qualification, Long>
+    implements QualificationRepository {
+    @Autowired
+    protected QualificationRepositoryImpl(final EntityManager entityManager) {
+        super(entityManager, Qualification.class);
+    }
+}
