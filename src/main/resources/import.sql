1e00e272f457cadf90c90a860963811c594183a9
TapiaX <luistapia2998@gmail.com>
2021-11-07 18:47:58 -0400

    datos de prueba SpaceAnswer

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -22,7 +22,10 @@ INSERT INTO users (ID, CREATED_AT, DELETED, UPDATED_AT, EMAIL) VALUES ('3', TIME
 -- ADVISERS
 INSERT INTO advisers (ID, FIRST_NAME, LAST_NAME)VALUES ( '1', 'Maria Leticia', 'Blanco Coca');
 
---COMPANIES
+-- CLASS_CODES
+INSERT INTO CLASS_CODES(ID, CREATED_AT, DELETED, UPDATED_AT, CODE, FK_ADVISER_ID) VALUES ('1', TIMESTAMP '2021-11-07 21:56:27.36174', 0, TIMESTAMP '2021-11-07T21:56:27.36175', 'jey-ibl-glp','1');
+
+-- COMPANIES
 INSERT INTO companies(ID, SHORT_NAME, NAME, COMPANY_TYPE, ADDRESS, FK_ADVISER_ID) VALUES ('2','ADASOFT','ADA SOFTWARE', 'SRL', 'Jordan y Oquendo', '1')
 INSERT INTO partners(ID,PARTNERS) VALUES ('2','Luis Tapia');
 INSERT INTO partners(ID,PARTNERS) VALUES ('2','Violeta Guzman');
@@ -48,7 +51,18 @@ INSERT INTO publications (ID, CREATED_AT, DELETED, UPDATED_AT, FK_ADVISER_ID, TI
 INSERT INTO publications (ID, CREATED_AT, DELETED, UPDATED_AT, FK_ADVISER_ID, TITLE, DATE, CODE, SEMESTER, FILE_URL, TYPE) VALUES ('5','2021-10-06 08:00:00',0,'2021-10-06 08:00:00','1','Título de una nueva convocatoria','2021-10-09 08:00:00','CPTIS-0910-2021','2-2021','https://drive.google.com/file/d/1Y1UxDtPa7Qr7uPzJ32D6pB7nwbCrrXxe/view?usp=sharing','0');
 INSERT INTO publications (ID, CREATED_AT, DELETED, UPDATED_AT, FK_ADVISER_ID, TITLE, DATE, CODE, SEMESTER, FILE_URL, TYPE) VALUES ('6','2021-10-06 08:00:00',0,'2021-10-06 08:00:00','1','Título de una nueva convocatoria','2021-10-10 08:00:00','CPTIS-1010-2021','2-2021','https://drive.google.com/file/d/1Y1UxDtPa7Qr7uPzJ32D6pB7nwbCrrXxe/view?usp=sharing','0');
 
---SPACES
+-- SPACES
 INSERT INTO spaces (ID,CREATED_AT, DELETED, UPDATED_AT) VALUES ('1','2021-10-06 08:00:00',0,'2021-10-06 08:00:00');
 INSERT INTO spaces (ID,CREATED_AT, DELETED, UPDATED_AT) VALUES ('2','2021-10-06 08:00:00',0,'2021-10-06 08:00:00');
 INSERT INTO spaces (ID,CREATED_AT, DELETED, UPDATED_AT) VALUES ('3','2021-10-06 08:00:00',0,'2021-10-06 08:00:00');
+
+-- SPACE_ANSWERS
+INSERT INTO space_answers (ID,CREATED_AT, DELETED, UPDATED_AT, FK_COMPANY_ID, FK_SPACE_ID) VALUES ('1', TIMESTAMP '2021-11-07 21:56:27.36174', 0, TIMESTAMP '2021-11-07T21:56:27.36175','2','1');
+INSERT INTO space_answers (ID,CREATED_AT, DELETED, UPDATED_AT, FK_COMPANY_ID, FK_SPACE_ID) VALUES ('2', TIMESTAMP '2021-11-07 21:56:27.36174', 0, TIMESTAMP '2021-11-07T21:56:27.36175','2','1');
+INSERT INTO space_answers (ID,CREATED_AT, DELETED, UPDATED_AT, FK_COMPANY_ID, FK_SPACE_ID) VALUES ('3', TIMESTAMP '2021-11-07 21:56:27.36174', 0, TIMESTAMP '2021-11-07T21:56:27.36175','3','1');
+
+-- FILES
+INSERT INTO files (ID, DELETED, NAME, URL) VALUES ('1', 0, 'AdaSoftParteA.pdf', 'https://firebasestorage.googleapis.com/v0/b/tis-storage.appspot.com/o/AdaSoftParteA.pdf?alt=media&token=abe66b00-508f-4821-a7fd-6d359a83fe4f');
+INSERT INTO files (ID, DELETED, NAME, URL) VALUES ('1', 0, 'AdaSoftParteA.pdf', 'https://firebasestorage.googleapis.com/v0/b/tis-storage.appspot.com/o/Fundempresa%20TIS.pdf?alt=media&token=df9e5f85-1e50-4434-aaa1-450375effff1');
+INSERT INTO files (ID, DELETED, NAME, URL) VALUES ('2', 0, 'AdaSoftParteA.pdf', 'https://firebasestorage.googleapis.com/v0/b/tis-storage.appspot.com/o/AdaSoftParteA.pdf?alt=media&token=abe66b00-508f-4821-a7fd-6d359a83fe4f');
+INSERT INTO files (ID, DELETED, NAME, URL) VALUES ('3', 0, 'acmeParteA.pdf', 'https://firebasestorage.googleapis.com/v0/b/tis-storage.appspot.com/o/AdaSoftParteA.pdf?alt=media&token=abe66b00-508f-4821-a7fd-6d359a83fe4f');
