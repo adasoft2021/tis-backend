8721deaff3d969b4fcf881b955013096118d95e9
Vivi Tolaba <liviatolabaveizaga@gmail.com>
2021-11-17 11:57:17 -0400

    TIS186 - Crear servidor de Project

--- src/main/java/com/adasoft/tis/services/ProjectService.java
+++ src/main/java/com/adasoft/tis/services/ProjectService.java
@@ -3,10 +3,13 @@ package com.adasoft.tis.services;
 import com.adasoft.tis.core.exceptions.EntityNotFoundException;
 import com.adasoft.tis.domain.Adviser;
 import com.adasoft.tis.domain.Project;
+import com.adasoft.tis.domain.Publication;
 import com.adasoft.tis.dto.project.CreateProjectDTO;
 import com.adasoft.tis.dto.project.ProjectResponseDTO;
+import com.adasoft.tis.dto.publication.PublicationResponseDTO;
 import com.adasoft.tis.repository.AdviserRepository;
-import com.adasoft.tis.repository.ProjectRepository
+import com.adasoft.tis.repository.ProjectRepository;
+import com.adasoft.tis.repository.PublicationRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -22,20 +25,34 @@ import static com.adasoft.tis.core.utils.Preconditions.checkArgument;
 public class ProjectService {
     private ProjectRepository projectRepository;
     private AdviserRepository adviserRepository;
+    private PublicationService publicationService;
+    private PublicationRepository publicationRepository;
     private ModelMapper projectMapper;
 
     public ProjectResponseDTO  create(final Long adviserId, final CreateProjectDTO projectDTO) {
         checkArgument(adviserId != null, "El id de Adviser no puede ser nulo.");
         checkArgument(projectDTO != null, "El ProjectDTO a crear no puede ser nulo.");
 
-        adviserRepository.findById(adviserId)
+        Adviser adviser = adviserRepository.findById(adviserId)
             .orElseThrow(() -> new EntityNotFoundException(Adviser.class, adviserId));
 
-        Project defaultProposal = projectMapper.map(projectDTO, Project.class);
+        Publication announcement = publicationRepository.findById(projectDTO.getCreateannouncementId())
+            .orElseThrow(() -> new EntityNotFoundException(Publication.class, projectDTO.getCreateannouncementId()));
+        Publication specificationSheet = publicationRepository.findById(projectDTO.getSpecificationSheetId())
+            .orElseThrow(() -> new EntityNotFoundException(Publication.class, projectDTO.getSpecificationSheetId()));
 
-        Project persistedProposal = projectRepository.save(defaultProposal);
+        Project defaultProject = projectMapper.map(projectDTO, Project.class);
+        defaultProject.setCreatedBy(adviser);
+        defaultProject.setAnnouncement(announcement);
+        defaultProject.setSpecificationSheet(specificationSheet);
+        Project persistedProject = projectRepository.save(defaultProject);
 
-        return projectMapper.map(persistedProposal, ProjectResponseDTO.class);
+        PublicationResponseDTO announcementResponseDTO = publicationService.findByID(projectDTO.getCreateannouncementId());
+        PublicationResponseDTO sheetResponseDTO = publicationService.findByID(projectDTO.getSpecificationSheetId());
+        ProjectResponseDTO res = projectMapper.map(persistedProject, ProjectResponseDTO.class);
+        res.setAnnouncement(announcementResponseDTO);
+        res.setSpecificationSheet(sheetResponseDTO);
+        return res;
     }
 
     public Collection<ProjectResponseDTO> getAllByAdviserId(final Long adviserId) {
