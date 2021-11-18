72885d059cefe265999f5c73dc73d7ca59f0d0c9
Leonardo-D-R-F <leonardo.roldan1139@gmail.com>
2021-11-18 09:40:55 -0400

    TIS-192 añadidos propiedades a la entidad espacio y DTOs

--- src/main/java/com/adasoft/tis/domain/Space.java
+++ src/main/java/com/adasoft/tis/domain/Space.java
@@ -1,14 +1,17 @@
 package com.adasoft.tis.domain;
 
 import com.adasoft.tis.core.domain.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
 
 @Data
 @SuperBuilder
@@ -18,5 +21,25 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "spaces")
 public class Space extends BaseEntity<Long> {
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Proyect proyect;
+
+    @Column(nullable = false)
+    private LocalDateTime limitDate;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Adviser createdBy;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "fk_proyect_id", nullable = false, updatable = false)
+    @NotNull
+    Space space;
 
 }
