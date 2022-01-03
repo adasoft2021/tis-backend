2a1b5898c56b43810c1614e43c55582dcdea80fd
TapiaX <luistapia2998@gmail.com>
2022-01-03 12:21:08 -0400

    recuperando servicio de Space para crear y listar

--- src/main/java/com/adasoft/tis/services/SpaceService.java
+++ src/main/java/com/adasoft/tis/services/SpaceService.java
@@ -3,12 +3,15 @@ package com.adasoft.tis.services;
 import com.adasoft.tis.core.exceptions.EntityNotFoundException;
 import com.adasoft.tis.domain.Adviser;
 import com.adasoft.tis.domain.Company;
-import com.adasoft.tis.domain.CompanySpace;
+
+import com.adasoft.tis.domain.Project;
 import com.adasoft.tis.domain.Space;
+import com.adasoft.tis.dto.space.CreateSpaceDTO;
 import com.adasoft.tis.dto.space.SpaceCompactResponseDTO;
 import com.adasoft.tis.dto.space.SpaceResponseDTO;
 import com.adasoft.tis.repository.AdviserRepository;
 import com.adasoft.tis.repository.CompanyRepository;
+import com.adasoft.tis.repository.ProjectRepository;
 import com.adasoft.tis.repository.SpaceRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -17,13 +20,42 @@ import org.springframework.stereotype.Service;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+import static com.adasoft.tis.core.utils.Preconditions.checkArgument;
+import static com.adasoft.tis.core.utils.Preconditions.checkUserId;
+
 @AllArgsConstructor
 @Service
+
 public class SpaceService {
-    private AdviserRepository adviserRepository;
     private SpaceRepository spaceRepository;
     private ModelMapper spaceMapper;
+    private AdviserRepository adviserRepository;
+    private ProjectRepository projectRepository;
     private CompanyRepository companyRepository;
+
+    public SpaceResponseDTO create(final CreateSpaceDTO spaceDTO, final Long adviserId) {
+        checkArgument(spaceDTO != null, "El SpaceDTO a crear no puede ser nulo.");
+
+        Adviser x = adviserRepository.findById(adviserId).orElseThrow(() ->
+                new EntityNotFoundException(Adviser.class, adviserId)
+        );
+
+        Project y = projectRepository.findById(spaceDTO.getProyectId()).orElseThrow(() ->
+                new EntityNotFoundException(Project.class, spaceDTO.getProyectId())
+        );
+
+
+        Space defaultSpace = spaceMapper.map(spaceDTO, Space.class);
+
+        defaultSpace.setCreatedBy(x);
+
+        defaultSpace.setProject(y);
+
+        Space persistedSpace = spaceRepository.save(defaultSpace);
+
+        return spaceMapper.map(persistedSpace, SpaceResponseDTO.class);
+
+    }
 
     public Collection<SpaceCompactResponseDTO> getAdviserSpaces(Long adviserId, Space.SpaceType spaceType) {
         adviserRepository.findById(adviserId).orElseThrow(() -> new EntityNotFoundException(Adviser.class, adviserId));
@@ -36,8 +68,11 @@ public class SpaceService {
     public Collection<SpaceCompactResponseDTO> getCompanySpaces(Long companyId, Space.SpaceType spaceType) {
         Company foundCompany = companyRepository.findById(companyId)
             .orElseThrow(() -> new EntityNotFoundException(Company.class, companyId));
-        Collection<Space> spaces = foundCompany.getAssigned().stream()
-            .map(CompanySpace::getSpace).collect(Collectors.toSet());
+        Collection<Space> spaces = spaceRepository.findByAdviser(foundCompany.getAdviser().getId(), spaceType);
+        if (foundCompany.getProject() != null)
+            spaces = spaces.stream().filter(space ->
+                    space.getProject().getId().equals(foundCompany.getProject().getId()))
+                .collect(Collectors.toSet());
         return spaces.stream().map(space -> spaceMapper.map(space, SpaceCompactResponseDTO.class))
             .collect(Collectors.toSet());
     }
@@ -47,5 +82,4 @@ public class SpaceService {
             .orElseThrow(() -> new EntityNotFoundException(Space.class, spaceId));
         return spaceMapper.map(space, SpaceResponseDTO.class);
     }
-
 }
