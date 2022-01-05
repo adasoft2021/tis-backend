8526df6023897c7cf4d60361c6ac2e06f14b1b55
jesusjimeneztapia <jesusjimeneztapia456@gmail.com>
2022-01-05 12:33:08 -0400

    Solucion al error del cors

--- src/main/java/com/adasoft/tis/config/CorsConfiguration.java
+++ src/main/java/com/adasoft/tis/config/CorsConfiguration.java
@@ -1,32 +1,39 @@
 package com.adasoft.tis.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-@Configuration
-public class CorsConfiguration {
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsConfiguration extends OncePerRequestFilter {
     @Value("${CLIENT_ORIGIN}")
     private String clientOrigin;
 
     @Value("${DEV_ORIGIN}")
     private String devOrigin;
 
-    @Bean
-    public WebMvcConfigurer getCorsConfiguration() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                    .allowedOrigins(clientOrigin, devOrigin)
-                    .allowedHeaders("*")
-                    .allowedMethods("*")
-                    .allowCredentials(true)
-                    .maxAge(3600);
-                WebMvcConfigurer.super.addCorsMappings(registry);
-            }
-        };
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request,
+                                    final HttpServletResponse response,
+                                    final FilterChain filterChain) throws ServletException, IOException {
+        String origin = request.getHeader("Origin");
+        if (origin.equals(clientOrigin) || origin.equals(devOrigin)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Token");
+        response.addHeader("Access-Control-Expose-Headers", "X-Token");
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, response);
+        }
     }
 }
